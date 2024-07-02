b8b28ac14feb6aa074f9df3ba62fa9bf044b8427
ankith88 <ankith88@gmail.com>
2024-07-03 08:06:43 +1000

    CU-86cvpmpy7

--- mp_ss_dashback_create_prod_order.js
+++ mp_ss_dashback_create_prod_order.js
@@ -45,7 +45,11 @@ function main() {
     var fuel_surcharge_to_be_applied = false;
 
     var oldSenderAddress = null;
+    var oldSenderSuburb = null;
+    var oldSenderPostcode = null;
+    var oldSenderEmail = null;
     var pickupJobCount = 0;
+    var pickupJobAt25DollarCount = 0;
 
     var todayDate = null;
     var tranDate = null;
@@ -83,6 +87,9 @@ function main() {
         var connote_number = searchResult.getValue("custrecord_connote_number");
         var barcode_source = searchResult.getValue("custrecord_barcode_source");
         var sender_address_1 = searchResult.getValue("custrecord_sender_address_1");
+        var sender_suburb = searchResult.getValue("custrecord_sender_suburb");
+        var sender_postcode = searchResult.getValue("custrecord_sender_post_code");
+        var sender_email = searchResult.getValue("custrecord_sender_email");
 
         var product_name = searchResult.getValue(
             "custrecord_cust_stock_prod_name");
@@ -181,31 +188,6 @@ function main() {
                 // productOrderRec.setFieldValue('custrecord_manual_barcode_count', manualBarcodesCount);
                 nlapiSubmitRecord(productOrderRec);
 
-                //Create invoice for the number of pickup jobs for the week. 
-                // recInvoice = nlapiCreateRecord('invoice', {
-                //     recordmode: 'dynamic'
-                // });
-                // recInvoice.setFieldValue('customform', 116);
-                // recInvoice.setFieldValue('entity', old_customer_id);
-                // recInvoice.setFieldValue('department', nlapiLoadRecord('partner',
-                //     old_zee_id).getFieldValue('department'));
-                // recInvoice.setFieldValue('location', nlapiLoadRecord('partner',
-                //     old_zee_id)
-                //     .getFieldValue('location'));
-                // recInvoice.setFieldValue('trandate', tranDate);
-                // recInvoice.setFieldValue('custbody_inv_date_range_from',
-                //     previousWeekStartDate);
-                // recInvoice.setFieldValue('custbody_inv_date_range_to', tranDate);
-                // recInvoice.selectNewLineItem('item');
-                // recInvoice.setCurrentLineItemValue('item', 'item', 10789);
-                // recInvoice.setCurrentLineItemValue('item', 'quantity',
-                //     pickupJobCount);
-                // recInvoice.commitLineItem('item');
-                // invoiceId = nlapiSubmitRecord(recInvoice);
-
-                // rasTeir1Count = 0;
-                // rasTeir2Count = 0;
-                // rasTeir3Count = 0;
 
                 var params = {
                     custscript_prev_deploy_create_prod_order: ctx.getDeploymentId(),
@@ -247,7 +229,11 @@ function main() {
             product_order_id = nlapiSubmitRecord(product_order_rec);
 
             if (oldSenderAddress != sender_address_1) {
-                pickupJobCount++;
+                if (oldSenderAddress == '31 Wheat Road' && oldSenderSuburb == 'Sydney' && oldSenderPostcode == '2000') {
+                    pickupJobAt25DollarCount++;
+                } else {
+                    pickupJobCount++;
+                }
             }
 
 
@@ -289,14 +275,6 @@ function main() {
                     'custrecord_ap_bill_item_description', 'Tier 3');
             }
 
-            // if (barcode_source == 1 || isNullorEmpty(barcode_source)) {
-            //     manual_surcharge_to_be_applied = true;
-            //     manualBarcodeCount++;
-            // } else {
-            //     manual_surcharge_to_be_applied = false;
-            //     digital_label++;
-            // }
-
             nlapiSubmitRecord(ap_stock_line_item);
 
             /**
@@ -314,7 +292,11 @@ function main() {
         } else {
 
             if (oldSenderAddress != sender_address_1) {
-                pickupJobCount++;
+                if (oldSenderAddress == '31 Wheat Road' && oldSenderSuburb == 'Sydney' && oldSenderPostcode == '2000') {
+                    pickupJobAt25DollarCount++;
+                } else {
+                    pickupJobCount++;
+                }
             }
 
             /**
@@ -360,15 +342,6 @@ function main() {
                     'custrecord_ap_bill_item_description', 'Tier 3');
             }
 
-            // if (barcode_source == 1 || isNullorEmpty(barcode_source)) {
-            //     manual_surcharge_to_be_applied = true;
-            //     manualBarcodeCount++;
-            // } else {
-            //     manual_surcharge_to_be_applied = false;
-            //     digital_label++;
-            // }
-
-
             nlapiSubmitRecord(ap_stock_line_item);
 
             /**
@@ -382,66 +355,6 @@ function main() {
                 'custrecord_cust_prod_stock_status', 7)
             nlapiSubmitRecord(cust_prod_stock_record);
 
-
-            /**
-             * Reschedule script after updating product order with AP Line Item an the count of line items created is 150
-             */
-            // if (count > 450) {
-            //     nlapiLogExecution('DEBUG', 'Count', count);
-
-            //     var productOrderRec = nlapiLoadRecord(
-            //         'customrecord_mp_ap_product_order', old_product_order_id);
-            //     if (manual_surcharge_to_be_applied == true) {
-            //         productOrderRec.setFieldValue(
-            //             'custrecord_manual_surcharge_applied', 1)
-            //     } else {
-            //         productOrderRec.setFieldValue(
-            //             'custrecord_manual_surcharge_applied', 2)
-            //     }
-
-            //     productOrderRec.setFieldValue('custrecord_ras_teir1_barcode_count', rasTeir1Count);
-            //     productOrderRec.setFieldValue('custrecord_ras_teir2_barcode_count', rasTeir2Count);
-            //     productOrderRec.setFieldValue('custrecord_ras_teir3_barcode_count', rasTeir3Count);
-            //     // productOrderRec.setFieldValue('custrecord_manual_barcode_count', manualBarcodesCount);
-            //     nlapiSubmitRecord(productOrderRec);
-
-            //     recInvoice = nlapiCreateRecord('invoice', {
-            //         recordmode: 'dynamic'
-            //     });
-            //     recInvoice.setFieldValue('customform', 116);
-            //     recInvoice.setFieldValue('entity', old_customer_id);
-            //     recInvoice.setFieldValue('department', nlapiLoadRecord('partner',
-            //         old_zee_id).getFieldValue('department'));
-            //     recInvoice.setFieldValue('location', nlapiLoadRecord('partner',
-            //         old_zee_id)
-            //         .getFieldValue('location'));
-            //     recInvoice.setFieldValue('trandate', tranDate);
-            //     recInvoice.setFieldValue('custbody_inv_date_range_from',
-            //         previousWeekStartDate);
-            //     recInvoice.setFieldValue('custbody_inv_date_range_to', tranDate);
-            //     recInvoice.selectNewLineItem('item');
-            //     recInvoice.setCurrentLineItemValue('item', 'item', 10789);
-            //     recInvoice.setCurrentLineItemValue('item', 'quantity',
-            //         pickupJobCount);
-            //     recInvoice.commitLineItem('item');
-            //     invoiceId = nlapiSubmitRecord(recInvoice);
-
-            //     // rasTeir1Count = 0;
-            //     // rasTeir2Count = 0;
-            //     // rasTeir3Count = 0;
-
-            //     var params = {
-            //         custscript_prev_deploy_create_prod_order: ctx.getDeploymentId(),
-            //     }
-
-            //     reschedule = rescheduleScript(prev_inv_deploy, adhoc_inv_deploy,
-            //         params);
-            //     nlapiLogExecution('AUDIT', 'Reschedule Return', reschedule);
-            //     if (reschedule == false) {
-
-            //         return false;
-            //     }
-            // }
         }
 
 
@@ -449,6 +362,9 @@ function main() {
         old_zee_id = cust_prod_zee;
         old_product_order_id = product_order_id
         oldSenderAddress = sender_address_1;
+        oldSenderSuburb = sender_suburb;
+        oldSenderPostcode = sender_postcode;
+        oldSenderEmail = sender_email;
         count++;
 
         return true;
@@ -487,8 +403,16 @@ function main() {
         recInvoice.setFieldValue('custbody_inv_date_range_to', tranDate);
         recInvoice.selectNewLineItem('item');
         recInvoice.setCurrentLineItemValue('item', 'item', 10789);
-        recInvoice.setCurrentLineItemValue('item', 'quantity',
-            pickupJobCount);
+        if (pickupJobAt25DollarCount > 0) {
+            recInvoice.setCurrentLineItemValue('item', 'quantity',
+                pickupJobAt25DollarCount);
+            recInvoice.setCurrentLineItemValue('item', 'rate',
+                25);
+        } else {
+            recInvoice.setCurrentLineItemValue('item', 'quantity',
+                pickupJobCount);
+        }
+
         recInvoice.commitLineItem('item');
         invoiceId = nlapiSubmitRecord(recInvoice);
     }
